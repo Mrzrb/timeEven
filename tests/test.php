a3d4699f5f245322390f08832610e0b0036e6229
张若彬(23117098) <zhangruobin001@ke.com>
2019-07-26 11:19:45 +0800

    comment test.php

--- tests/test.php
+++ tests/test.php
@@ -2,18 +2,18 @@
 
 require '../vendor/autoload.php';
 
-use RobinSchedule\TimeEven;
-use RobinSchedule\TimeEvenJob;
+//use RobinSchedule\TimeEven;
+//use RobinSchedule\TimeEvenJob;
 
-$config = [
-	'host' => 'localhost',
-	'port' => '6379',
-];
-$app = new TimeEven(null, $config);
+//$config = [
+	//'host' => 'localhost',
+	//'port' => '6379',
+//];
+//$app = new TimeEven(null, $config);
 
-$job = (new TimeEvenJob())->withPayload(['test' => 123]);
+//$job = (new TimeEvenJob())->withPayload(['test' => 123]);
 
-$app->dispatch($job);
+//$app->dispatch($job);
 
 //$app->work('test-job', 3);
 //$app->schedule();
